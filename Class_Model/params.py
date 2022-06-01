db270307adf70e95cfbe2e6cd95f38a3c3b6e489
Toby Winter <tobywinter1@gmail.com>
2022-06-01 11:48:19 +0200

    Toby - add model + params.py

---
+++ Class_Model/params.py
@@ -0,0 +1,51 @@
+### MLFLOW configuration - - - - - - - - - - - - - - - - - - -
+
+MLFLOW_URI = "https://mlflow.lewagon.ai/"
+EXPERIMENT_NAME = "[NL] [Amsterdam] [Snowingg] Taxis + 1"
+
+### DATA & MODEL LOCATIONS  - - - - - - - - - - - - - - - - - - -
+
+PATH_TO_LOCAL_MODEL = 'model.joblib'
+
+BUCKET_TEST_DATA_PATH = "data/test.csv"
+
+PATH_TO_MODEL = 'models/taxifarepipe/model.joblib'
+
+
+### GCP configuration - - - - - - - - - - - - - - - - - - -
+
+# /!\ you should fill these according to your account
+
+### GCP Project - - - - - - - - - - - - - - - - - - - - - -
+
+# not required here
+
+### GCP Storage - - - - - - - - - - - - - - - - - - - - - -
+
+BUCKET_NAME = 'wagon-data-907-deeppv'
+
+##### Data  - - - - - - - - - - - - - - - - - - - - - - - -
+
+# train data file location
+# /!\ here you need to decide if you are going to train using the provided and uploaded data/train_1k.csv sample file
+# or if you want to use the full dataset (you need need to upload it first of course)
+
+#TODO BUCKET_TRAIN_DATA_PATH = ''
+
+##### Training  - - - - - - - - - - - - - - - - - - - - - -
+
+# not required here
+
+##### Model - - - - - - - - - - - - - - - - - - - - - - - -
+
+# model folder name (will contain the folders for all trained model versions)
+MODEL_NAME = 'ChinaModel'
+
+# model version folder name (where the trained model.joblib file will be stored)
+MODEL_VERSION = 'v1'
+
+### GCP AI Platform - - - - - - - - - - - - - - - - - - - -
+
+# not required here
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - -
