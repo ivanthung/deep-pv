0efea357f6d15e59794f8ab2972cc8794517b3d1
Toby Winter <tobywinter1@gmail.com>
2022-06-02 16:30:43 +0200

    updating api and streamlit

--- deep_pv/get_data.py
+++ deep_pv/get_data.py
@@ -8,18 +8,17 @@ from deep_pv.params import BUCKET_NAME, BUCKET_TRAIN_DATA_PATH, IMG_SIZE, BATCH_
 
 #get image for prediction from GCP
 #saved locally as "file_name.png"
-def get_predict_image_gcp(file_name):
+def get_predict_image_gcp(file_name, image_type = 'jpg'):
     """method to get the training data (or a portion of it) from google cloud bucket"""
     # Add Client() here
     client = storage.Client()
-    path = f"gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}/{file_name}.png"
+    path = f"gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}/{file_name}.{image_type}"
     #prepare data into train and test sets
     bucket = client.bucket(BUCKET_NAME)
-    print(bucket)
-    blob = bucket.blob(f'{BUCKET_TRAIN_DATA_PATH}/{file_name}.png')
-    blob.download_to_filename('{file_name}.png')
-    return '{file_name}.png'
+    blob = bucket.blob(f'{BUCKET_TRAIN_DATA_PATH}/{file_name}.{image_type}')
+    blob.download_to_filename(f'{file_name}.{image_type}')
+    return f'{file_name}.{image_type}'
 
     #return train_ds, test_ds
 if __name__ =="__main__":
-    get_predict_image_gcp()
+    get_predict_image_gcp('51.906771_4.451552')
--- deep_pv/params.py
+++ deep_pv/params.py
@@ -31,7 +31,7 @@ BUCKET_NAME = 'wagon-data-907-deeppv'
 # /!\ here you need to decide if you are going to train using the provided and uploaded data/train_1k.csv sample file
 # or if you want to use the full dataset (you need need to upload it first of course)
 
-#TODO BUCKET_TRAIN_DATA_PATH = ''
+BUCKET_TRAIN_DATA_PATH = 'data/Rotterdam/PV Present'
 
 ##### Training  - - - - - - - - - - - - - - - - - - - - - -
 
