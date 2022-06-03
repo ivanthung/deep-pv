c4c6488494c70a5d3870b659fb01333c8ae2bd6f
Ivan Thung <ivanthung@gmail.com>
2022-06-03 10:57:40 +0200

    test_outputs

---
+++ deep_pv/utils/test_output.py
@@ -0,0 +1,11 @@
+import pickle
+import matplotlib.pyplot as plt
+
+file_name = 'test_results/sample1.pkl'
+
+with open(file_name, 'rb') as handle:
+    results = pickle.load(handle)
+
+
+mask = results[0][0]['masks'][:,:,0]
+plt.imshow(mask)
